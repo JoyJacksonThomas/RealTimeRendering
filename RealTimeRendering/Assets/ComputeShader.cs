590549293615660af5d2e9372c831590bd616f33
tommy <thomas.mann@mymail.champlain.edu>
2020-04-12 21:02:15 -0400

    commented out compute shader so it could work

--- RealTimeRendering/Assets/ComputeShader.cs
+++ RealTimeRendering/Assets/ComputeShader.cs
@@ -1,35 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class ComputeShader : ShaderScript
-{
-    public ComputeShader Shader;
-    public string kernelName = "CSMain";
-    public RenderTexture RenderTex;
+//public class ComputeShader : ShaderScript
+//{
+//    public ComputeShader Shader;
+//    public string kernelName = "CSMain";
+//    public RenderTexture RenderTex;
 
-    // Use this for initialization
+//    // Use this for initialization
 
-    private int _kernel;
-    private RenderTexture _renderTex;
-    void Awake()
-    {
-        _kernel = Shader.FindKernel(kernelName);
+//    private int _kernel;
+//    private RenderTexture _renderTex;
+//    void Awake()
+//    {
+//        _kernel = Shader.FindKernel(kernelName);
 
-        //_renderTex = new RenderTexture (512, 512, 24);
-        _renderTex = RenderTex;
-        _renderTex.enableRandomWrite = true;
-        //_renderTex.Create ();
+//        //_renderTex = new RenderTexture (512, 512, 24);
+//        _renderTex = RenderTex;
+//        _renderTex.enableRandomWrite = true;
+//        //_renderTex.Create ();
 
-        ExecComputeShader();
-    }
+//        ExecComputeShader();
+//    }
 
 
-    public void ExecComputeShader()
-    {
+//    public void ExecComputeShader()
+//    {
 
-        Shader.SetTexture(_kernel, "Result", _renderTex);
-        Shader.SetInt("TimeValue", (int)Time.timeSinceLevelLoad * 100);
-        Shader.Dispatch(_kernel, 512 / 8, 512 / 8, 1);
-    }
-}
+//        Shader.SetTexture(_kernel, "Result", _renderTex);
+//        Shader.SetInt("TimeValue", (int)Time.timeSinceLevelLoad * 100);
+//        Shader.Dispatch(_kernel, 512 / 8, 512 / 8, 1);
+//    }
+//}
